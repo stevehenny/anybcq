8eaac905d48c3a00cb9e3a81eb24d118a1a6b915
stevehenny <stephen.henstrom@gmail.com>
2026-03-14 19:27:15 -0500

    fixing anybcq transformer dependency

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import find_packages, setup
 
 install_requires = [
-    'torch',
-    'numpy',
-    'transformers==4.51.0',
-    'datasets',
-    'evaluate',
-    'lm_eval==0.4.5',
-    'peft==0.10.0'
+    "torch",
+    "numpy",
+    "transformers==4.56.0",
+    "datasets",
+    "evaluate",
+    "lm_eval==0.4.5",
+    "peft==0.10.0",
 ]
 
 
 setup(
-    name='anybcq',
-    version='0.0',
-    author='Anonymous',
-    author_email='Anonymous',
-    description='Repository for anybcq research',
+    name="anybcq",
+    version="0.0",
+    author="Anonymous",
+    author_email="Anonymous",
+    description="Repository for anybcq research",
     packages=find_packages(),
     install_requires=install_requires,
 )
